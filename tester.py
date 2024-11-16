1ba3577d064f372b99b29eb1022406ec5e20514e
Jimuk-hub <dlawlanr0187@naver.com>
2024-11-16 20:42:25 +0900

    Update tester.py

--- tester.py
+++ tester.py
@@ -1,11 +1,21 @@
 import math
 
 def pythagoras(a, b):
-  c = math.sqrt(a**2 + b**2)
-  print('c = ', c)
-  return c
+
+    c = math.sqrt(a**2 + b**2)
+    print(f'c = {c}')
+    return c
 
 def circle(r):
-  area = math.pi * r**2
-  print('area = ', area)
-  return area
+    
+    area = math.pi * r**2
+    print(f'area = {area}')
+    return area
+
+
+if __name__ == "__main__":
+
+    pythagoras(3, 4)  
+
+    
+    circle(10)  
