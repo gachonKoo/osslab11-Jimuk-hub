aac8e0562be07297f9e8a705c496c4c4acf3b66f
Jimuk-hub <dlawlanr0187@naver.com>
2024-11-16 20:32:52 +0900

    Create tester.py

---
+++ tester.py
@@ -0,0 +1,14 @@
+import geo.uitils as utils
+import math
+
+def pythagoras (a, b):
+  a, b = 3, 4
+  c = math.sqrt(3**2+4**2)
+  return c
+  print('c = ', c)
+
+def circle(r):
+  r = 10
+  area = matt.pi * 10**2
+  return area
+  print('area = ', area)
